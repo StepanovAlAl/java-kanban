24ad6c4350161ff234642a5fc2808752df3ed421
Vandal <arhvandal@yandex.ru>
2025-05-29 19:05:41 +0300

    9Sprint Code review fix

--- test/manager/http/HistoryHandlerTest.java
+++ test/manager/http/HistoryHandlerTest.java
@@ -1,5 +1,6 @@
 package manager.http;
 
+import com.google.gson.reflect.TypeToken;
 import manager.InMemoryTaskManager;
 import manager.TaskManager;
 import model.Task;
@@ -12,7 +13,10 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
+import static manager.http.BaseHttpHandler.gson;
 import static org.junit.jupiter.api.Assertions.*;
 
 class HistoryHandlerTest {
@@ -40,6 +44,15 @@ class HistoryHandlerTest {
 
     @Test
     void getHistory_shouldReturnHistory() throws IOException, InterruptedException {
+        // 1. Подготовка
+        manager.deleteAllTasks();
+        manager.deleteAllEpics();
+        manager.deleteAllSubtasks();
+        Task testTask = new Task("Test Task", "Description");
+        int taskId = manager.createTask(testTask);
+        manager.getTaskById(taskId);
+
+        // 2. Выполнение запроса
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("http://localhost:8080/history"))
                 .GET()
@@ -47,7 +60,21 @@ class HistoryHandlerTest {
 
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
+        // 3. Проверки
         assertEquals(200, response.statusCode());
         assertTrue(response.body().contains("Test"));
+        // Новое - преобразуем JSON в задачи
+        List<Task> history = gson.fromJson(
+                response.body(),
+                new TypeToken<ArrayList<Task>>() {
+                }.getType()
+        );
+        // Сравниваем
+        assertEquals(1, history.size(), "История должна содержать 1 задачу");
+
+        Task historyTask = history.get(0);
+        assertEquals(taskId, historyTask.getId(), "ID задачи не совпадает");
+        assertEquals("Test Task", historyTask.getName(), "Название задачи не совпадает");
+        assertEquals("Description", historyTask.getDescription(), "Описание задачи не совпадает");
     }
 }
